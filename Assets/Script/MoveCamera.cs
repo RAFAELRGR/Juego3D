831848b77a9ac6d621dfe5a2293ad9ef7afd1615
RAFAELRGR <rafaelronaldog@gmail.com>
2024-03-15 12:34:29 -0500

    add(mensaje prueba)

--- Assets/Script/MoveCamera.cs
+++ Assets/Script/MoveCamera.cs
@@ -33,5 +33,8 @@ public class MoveCamera : MonoBehaviour
         //applying both rotations
         transform.localRotation = Quaternion.Euler(xRotation, YRotation, 0f);
 
+        //Sebastian es un sabiondo 
+
+
     }
 }
